c9088c47064cdf367dfe499fa21204acf915d4e1
Emily <14233135+emilyyyyy@user.noreply.gitee.com>
2024-11-11 21:14:14 +0800

    1

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+/**
+ * @description:
+ *
+ * 给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "bcabc"
+ * 输出："abc"
+ * 示例 2：
+ *
+ * 输入：s = "cbacdcbc"
+ * 输出："acdb"
+ *
+ * 1 <= s.length <= 104
+ * s 由小写英文字母组成
+ */
+class Solution2 {
+    public String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a']--;
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        String input = "bcabc"; // 可以替换为其他输入测试
+        String result = solution.removeDuplicateLetters(input);
+        System.out.println("Result: " + result); // 输出结果
+    }
+
+}
+
